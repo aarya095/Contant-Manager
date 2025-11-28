ff1f296cdf4a39f1e57714bbd779a45cff9890da
Aarya Sarfare <aarya.work005@gmail.com>
2025-11-28 13:46:09 +0530

    debugging and refactoring after adding encryption completed

--- modules/operations.py
+++ modules/operations.py
@@ -63,6 +63,9 @@ def view_contact():
         print(f"    Email: {original_email}")
         index += 1
     conn.close()
+    already_existing_names_list = get_and_validate.generate_list_of_already_used_names()
+    if len(already_existing_names_list) == 0:
+        print("No Entries!")
 
 def update_contact():
     """Updates the contact information"""
@@ -84,7 +87,7 @@ def update_contact():
         if update_name_input in already_existing_names_list:
             break
         elif update_name_input not in already_existing_names_list:
-            print("Enter a name which is already in the list!")
+            print("Error! Enter a name which is already in the list!")
 
     name_input = get_and_validate.get_and_validate_input_name()
     contact_num_input = get_and_validate.get_and_validate_input_number()
@@ -134,7 +137,7 @@ def delete_contact():
         if name_input in already_existing_names_list:
             break
         elif name_input not in already_existing_names_list:
-            print("Enter a name which is already in the list!")
+            print("Error! Enter a name which is already in the list!")
 
     cur.execute("delete from contacts where name = ?", (name_input,))
     conn.commit()
@@ -147,31 +150,50 @@ def search_name():
     from the list of already existing names and get the contact information"""
 
     already_existing_names_list = get_and_validate.generate_list_of_already_used_names()
-    user_input_for_search = input("Enter the name you want to search: ")
+    if len(already_existing_names_list) == 0:
+        print("No Entries!")
+    else:
+        user_input_for_search = input("Enter the name you want to search: ")
 
-    matched_cases_list = [] # list to store the names where input has matched 
-    for name in already_existing_names_list:
-        # Loops through the already_existing_names_list and matching using re.search function
-        match_case = re.search(user_input_for_search, name, re.IGNORECASE) # Ignoring upper/lower case for better chances       
-        if match_case:
-            matched_cases_list.append(name)
-        if match_case == None:
-            pass
-    if matched_cases_list == []:
-        print("No matches found.")
+        matched_cases_list = [] # list to store the names where input has matched 
+        for name in already_existing_names_list:
+            # Loops through the already_existing_names_list and matching using re.search function
+            match_case = re.search(user_input_for_search, name, re.IGNORECASE) # Ignoring upper/lower case for better chances       
+            if match_case:
+                matched_cases_list.append(name)
+            if match_case == None:
+                pass
+        if matched_cases_list == []:
+            print("No matches found.")
 
-    conn = db.connect_db()
-    cur = conn.cursor()
+        conn = db.connect_db()
+        cur = conn.cursor()
 
-    for name in matched_cases_list:
-        cur.execute("select * from contacts where name = ?",(name,))
-        contact_data = cur.fetchone()
-        print("\nResults:")
-        print(f"\nName: {contact_data[0]}")
-        print(f"Contact Number: {contact_data[1]}")
-        print(f"Email: {contact_data[2]}")
+        for name in matched_cases_list:
 
-    conn.close()
+            cur.execute("select * from contacts where name = ?",(name,))
+            contact_data = cur.fetchone()
+
+            #Retrieves the contact number and decrypts it via its key in .env file
+            key_for_contact_num = en.retrieve_contact_num_key_from_env_file(name)
+            encrypted_contact_num_tuple = en.retrieve_encrypted_number_from_db(name)
+            encrypted_contact_num = encrypted_contact_num_tuple[0]
+            original_contact_num = en.decrypt(encrypted_contact_num, key_for_contact_num)
+            original_contact_num = original_contact_num.decode('utf-8') # decodes the contact number otherwise it shows b'contact number' which is not preferred
+
+            #Retrieves the email and decrypts it via its key in .env file
+            key_for_email = en.retrieve_email_key_from_env_file(name)
+            encrypted_email_tuple = en.retrieve_encrypted_email_from_db(name)
+            encrypted_email = encrypted_email_tuple[0]
+            original_email = en.decrypt(encrypted_email, key_for_email)
+            original_email = original_email.decode('utf-8') # decodes the email otherwise it shows b'email' which is not preferred
+
+            print("\nResults:")
+            print(f"\nName: {contact_data[0]}")
+            print(f"Contact Number: {original_contact_num}")
+            print(f"Email: {original_email}")
+
+        conn.close()
         
 if __name__ == '__main__':
     search_name()
