85ef3093d5bd923e2e1dc212176287b578ea5b60
Aarya Sarfare <aarya.work005@gmail.com>
2026-03-22 12:56:43 +0530

    adding multiple .env files for different branches

--- app/database/database.py
+++ app/database/database.py
@@ -1,8 +1,10 @@
+import psycopg2
 import os
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 
-load_dotenv()
+env_file = ".env.main"
+load_dotenv(dotenv_path=env_file)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
